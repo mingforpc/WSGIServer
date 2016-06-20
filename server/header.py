d4c503089c08d6e27d13830bb61329cc2aebaf8d
jeff <jianmingforpc@gmail.com>
2016-06-20 17:28:36 +0800

    prepare to write a header class;

--- server/header.py
+++ server/header.py
@@ -40,63 +40,112 @@ def format_date_time(timestamp):
     )
 
 
-class ResponseHeaders(object):
-    """ The class to manage the response headers """
-    def __init__(self, headers):
-        if type(headers) is not ListType:
-            raise TypeError("Header must be a list of name/value tuples")
-
-        self.__headers = headers
+class Headers(object):
+    """ The basic header class to manage the request or response header """
+    def __init__(self):
+        self.headers = {}
 
     def __len__(self):
         """ return the number of headers """
-        return len(self.__headers)
+        return len(self.headers) if self.headers is not None else 0
 
     def __setitem__(self, key, value):
-        """ Set a header """
-        del self[key]
-        self.__headers.append((key, value))
-
-    def __delitem__(self, key):
-        """ Delete a header """
-        key = key.lower()
-        self.__headers[:] = [kv for kv in self.__headers if kv[0].lower != key]
+        """ set a header """
+        self.headers[key] = value
 
     def __getitem__(self, item):
-        """ Return the value of the header by key.
-         If no such header, return NULL
-         """
-        return self.get(item)
+        """ get a header, if not exist than return None """
+        return self.headers.get(item)
 
-    def get(self, name, default=None):
-        """ Get the header by name , if not exist reutn 'default' """
-        name = name.lower()
-        for k, v in self.__headers:
-            if k.lower() == name:
-                return v
-        return default
+    def __delitem__(self, key):
+        """ remove a header """
+        if self.headers.get(key) is not None:
+            del self.headers[key]
 
-    def has_key(self, name):
-        """ Retrun ture if has this header """
-        name = name.lower()
-        return self.get(name) is not None
-
-    __contains__ = has_key
+    def get(self, header, default=None):
+        """
+        Get a header value, if header not exist return the default value
+        :param header: the header name
+        :param default: the default value
+        :return:
+        """
+        return self.headers.get(header, default)
 
     def keys(self):
-        return [k for k, v in self.__headers]
+        """
+        return the list of header name
+        :return:
+        """
+        return self.headers.keys()
 
-    def values(self):
-        return [v for k, v in self.__headers]
+    def set_header(self, key, value):
+        """ set a header"""
+        self.headers[key] = value
 
-    def items(self):
-        return self.__headers[:]
-
-    def add_header(self, name, value):
-        if self.has_key(name):
-            del self.__headers[name]
-
-        self.__headers.append((name, value))
+    def has_key(self, key):
+        """ return True if has this header else return False """
+        return self.headers.has_key(key)
 
     def __str__(self):
-        return '\r\n'.join(["%s: %s" % kv for kv in self.__headers] + ['', ''])
+        return '\r\n'.join(["%s: %s" % (k, v) for k, v in self.headers.items()] + ['', ''])
+
+        # class ResponseHeaders(object):
+#     """ The class to manage the response headers """
+#     def __init__(self, headers):
+#         if type(headers) is not ListType:
+#             raise TypeError("Header must be a list of name/value tuples")
+#
+#         self.__headers = headers
+#
+#     def __len__(self):
+#         """ return the number of headers """
+#         return len(self.__headers)
+#
+#     def __setitem__(self, key, value):
+#         """ Set a header """
+#         del self[key]
+#         self.__headers.append((key, value))
+#
+#     def __delitem__(self, key):
+#         """ Delete a header """
+#         key = key.lower()
+#         self.__headers[:] = [kv for kv in self.__headers if kv[0].lower != key]
+#
+#     def __getitem__(self, item):
+#         """ Return the value of the header by key.
+#          If no such header, return NULL
+#          """
+#         return self.get(item)
+#
+#     def get(self, name, default=None):
+#         """ Get the header by name , if not exist reutn 'default' """
+#         name = name.lower()
+#         for k, v in self.__headers:
+#             if k.lower() == name:
+#                 return v
+#         return default
+#
+#     def has_key(self, name):
+#         """ Retrun ture if has this header """
+#         name = name.lower()
+#         return self.get(name) is not None
+#
+#     __contains__ = has_key
+#
+#     def keys(self):
+#         return [k for k, v in self.__headers]
+#
+#     def values(self):
+#         return [v for k, v in self.__headers]
+#
+#     def items(self):
+#         return self.__headers[:]
+#
+#     def add_header(self, name, value):
+#         if self.has_key(name):
+#             del self.__headers[name]
+#
+#         self.__headers.append((name, value))
+#
+#     def __str__(self):
+#         return '\r\n'.join(["%s: %s" % kv for kv in self.__headers] + ['', ''])
