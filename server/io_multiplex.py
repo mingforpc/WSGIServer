09a612f32b1465eed2111738457e00799f5c4c7e
jeffzhuo <jianmingforpc@gmail.com>
2016-06-29 23:17:24 +0800

    add io

---
+++ server/io_multiplex.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014-2015 jeffZhuo
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import with_statement
+from __future__ import nested_scopes
+from __future__ import generators
+
+try:
+    from select import epoll as multiplexer
+except:
+    from select import select as multiplexer
